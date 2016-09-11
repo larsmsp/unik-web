db4b6ca3f6ebe6fb51f462ab5017f2e89b0e8fac
Kristian Saksvik Munkvold <Kristian.Saksvik.Munkvold@computas.com>
2016-09-11 20:31:41 +0200

    Begun implementing new design for teaser-page.

--- client/navbar.js
+++ client/navbar.js
@@ -1,30 +1,31 @@
 var NavbarItem = React.createClass({
-  render: function() {
-    return React.createElement('a', {className: 'navbar-brand', href: this.props.link}, this.props.name);
-  }
+    render: function () {
+        return React.createElement('a', {className: 'navbar-brand', href: this.props.link}, this.props.name);
+    }
 });
 
 var Navbar = React.createClass({
-  render: function() {
-    return React.createElement('nav', {className: 'navbar navbar-inverse navbar-fixed-top'},
-      React.createElement('div', {className: 'container'},
-        React.createElement('div', {className: 'navbar-header'},
-          this.props.items.map(function (item) {
-            return React.createElement(NavbarItem, item);
-          })
-        )
-      )
-    );
-  }
+    render: function () {
+        return React.createElement('nav', {className: 'navbar navbar-inverse navbar-static-top'},
+            React.createElement('div', {className: 'col-xs-12'},
+                React.createElement('div', {className: 'navbar-header'},
+                    React.createElement('a', {className: 'navbar-brand', href: '/'},
+                        React.createElement('img', {className: 'header-logo', src: 'images/computas-logo.png'})),
+                    this.props.items.map(function (item) {
+                        return React.createElement(NavbarItem, item);
+                    })
+                )
+            )
+        );
+    }
 });
 
 var items = [
-  {link: '/', name:'UNIK'},
-  {link: '/badges.html', name:'Merker'},
-  {link: '/leaderboard.html', name:'Poengtavle'}
+    // {link: '/badges.html', name: 'Merker'},
+    // {link: '/leaderboard.html', name: 'Poengtavle'}
 ];
 
 ReactDOM.render(
-  React.createElement(Navbar, {items: items}),
-  document.getElementById('navbar')
+    React.createElement(Navbar, {items: items}),
+    document.getElementById('navbar')
 );
