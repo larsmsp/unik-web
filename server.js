547659a1f255d0bd8866a4d94647c0df10e75f84
larsmsp <lars@atbrox.com>
2017-03-08 15:13:15 +0100

    Removed what is not needed from the IoT challenge

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var express = require('express'),
 	app = express(),
 	http = require('http').Server(app),
-	io = require('socket.io')(http),
-	CNST = require('./constants.js'),
-	twitter = require('./twitter.js');
 
-	teams = CNST.TEAMS,
 	port = process.env.PORT || 5000,
 	// Maybe use 6000 for production. Rate limit is 180 calls in 15 minutes (every 5000 ms).
 	interval = process.env.TIME_INTERVAL || 6000;
@@ -25,21 +21,6 @@ app.get('/admin', function (req, res) {
 });
 
 app.use('/node_modules', express.static('node_modules'));
-
-io.on('connect', (socket) => {
-    io.emit('display:teams', teams);
-    io.emit('display:badges', CNST.BADGES);
-    io.emit('display:runningContest', CNST.RUNNING_CONTEST);
-});
-
-var updateScores = () => {
-	twitter.updateAllTeamScores((updatedTeams, noOfCalls) => {
-	    teams = updatedTeams;
-	    io.emit('display:teams', teams);
-		setTimeout(updateScores, interval * noOfCalls);
-	});
-};
-updateScores();
 
 http.listen(port, () => {
     console.log('Server running on http://localhost:' + port + '/');
